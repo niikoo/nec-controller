6f0e973cd03e2afc9e0d397678709f139ce1aba6
Nikolai Ommundsen <post@niikoo.net>
2018-04-27 00:32:50 +0200

    Port scanning to find projector

--- index.ts
+++ index.ts
@@ -1,4 +1,80 @@
 import * as net from 'net';
+import * as os from 'os';
+import * as raw from 'raw-socket';
+import * as portscanner from 'portscanner';
+
+function byteToHexString(uint8arr: Uint8Array): string {
+    if (!uint8arr) {
+      return undefined;
+    }
+    var hexStr = '';
+    for (var i = 0; i < uint8arr.length; i++) {
+      var hex = (uint8arr[i] & 0xff).toString(16);
+      hex = (hex.length === 1) ? '0' + hex : hex;
+      hexStr += '0x' + hex.toUpperCase() + ' ';
+    }
+    
+    return hexStr;
+  }
+
+/**
+ * Get base net from ip, e.g. 192.168.10.22 -> 192.168.10.
+ * @param ip IP Address, e.g. 192.168.1.1
+ */
+function getBaseAddr(ip: string): string {
+    let parts = ip.split('.');
+    parts[3] = '';
+    return parts.join('.');
+}
+
+let netInterfaces = {};
+try {
+    netInterfaces = os.networkInterfaces();
+} catch(ex) {
+    console.error('Could not get net interfaces', ex);
+}
+let subnets = [];
+for(let inf of Object.keys(netInterfaces)) {
+    for(let part of netInterfaces[inf]) {
+        if(!part.address.includes(':') && part.address !== '127.0.0.1') { // Skip IPv6 & localhost
+            subnets.push(getBaseAddr(part.address));   
+        }
+    }
+}
+
+console.log(subnets);
+
+let necProjectors = [];
+
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const scanSubnet = async(baseIP: string) => {
+    for(let i = 0; i <= 255; i++) {
+        console.log('Scanning for port 7142 open on IP: ' + baseIP + i);
+        try {
+            let status = await portscanner.checkPortStatus(7142, baseIP + i);
+            console.log('-----> ' + status);
+            if(status === 'open') {
+                necProjectors.push(baseIP + i);
+            }
+        } catch(ex) {
+            console.error('Check port open error: ', ex);
+        }
+        
+    }
+};
+
+const scanSubnets = async() => {
+    for(let subnet of subnets) {
+        console.log('Scanning subnet: ', subnet + '0/24');
+        await scanSubnet(subnet)
+        console.log('Found nec projectors: ', necProjectors);
+        await snooze(1000);
+    }
+}
+
+// scanSubnets();
+// console.log('[TOTAL] Found nec projectors: ', necProjectors);
 
 export enum SoundFunction {
     notAvailable = 0x00,
@@ -20,6 +96,10 @@ const commands = {
         mute: {
             on: [0x02, 0x10, 0x00, 0x00, 0x00, 0x12],
             off: [0x02, 0x11, 0x00, 0x00, 0x00, 0x13]
+        },
+        freeze: {
+            on: [0x01,0x98,0x00,0x00,0x01,0x01,0x9B],
+            off: [0x01,0x98,0x00,0x00,0x01,0x02,0x9C]
         }
     },
     sound: {
@@ -75,24 +155,73 @@ const errorCodeList = {
     }
 }
 
+let byteArrayToNumberArray = (byteArray: Uint8Array | Uint16Array | Uint32Array): number[] => {
+    var value = [];
+    for ( var i = byteArray.length - 1; i >= 0; i--) {
+        value.push(byteArray[i]);
+    }
+    return value;
+};
 
-function toHexArray(byteArray) {
+function toHexArray(byteArray): number[] {
     return Array.prototype.map.call(byteArray, function(byte) {
       return ('0' + (byte & 0xFF).toString(16)).slice(-2);
     });
-}
+} 
 
-let socket: net.Socket = new net.Socket({
-    allowHalfOpen: true,
-    readable: true,
-    writable: true
-}).connect({
-    host: '192.168.10.21',
-    port: 7142
-}, () => {
-    console.log('Connected, trying to send read state command');
-    socket.write(new Uint8Array(commands.status.runningStatusRequest));
-});
+/*let udpSocket: dgram.Socket = dgram.createSocket('udp4', (msg, rinfo) => {
+    console.log('Socket info', msg, rinfo);
+});*/
+
+
+const checkProjectorAvailable = async(ip, port) => {
+    let status = await portscanner.checkPortStatus(port, ip);
+    console.log('Projector status: ', status);
+    if(status === 'open') {
+        return true; 
+    }
+    return false;
+};
+
+let connectToProjector = true; // checkProjectorAvailable('192.168.1.121', 7142);
+console.log('Use connect to projector?', connectToProjector);
+/*
+if(!connectToProjector) {
+    // Use raw socket to wake projector
+    let rawSocket = raw.createSocket({protocol: 6});
+    const buffer = new Buffer(commands.power.on);
+    let socketLevel = raw.SocketLevel.IPPROTO_IP;
+    let socketOption = raw.SocketOption.IP_TTL;
+    const beforeSend = () => {
+        rawSocket.setOption(socketLevel, socketOption);
+    }
+    rawSocket.send(buffer, 0, buffer.length, '192.168.1.121');
+}*/
+
+
+let socket: net.Socket;
+try {
+    if(connectToProjector)
+    socket = new net.Socket({
+        allowHalfOpen: false,
+        readable: true,
+        writable: true
+    }).connect(<net.TcpSocketConnectOpts>{
+        port: 7142,
+        family: 4,
+        host: '192.168.1.121',
+        lookup: () => { return true; }
+    }, () => {
+        console.log('Connected, trying to send read state command');
+        socket.write(new Uint8Array(commands.power.off));
+        setInterval(() => {
+            socket.write(new Uint8Array(commands.status.runningStatusRequest));
+        }, 5000);
+        
+    });
+} catch(ex) {
+    console.error('Failed to connect to projector', ex);
+}
 
 /**
  * The value of the "Control ID" set for the projector is used.
@@ -149,7 +278,11 @@ export class ProjectorController {
     }
 
     setError(err1: number, err2: number, errMessage: string) {
-        console.error('Error: ', err1, err2, errMessage);
+        if(errorCodeList[err1] && errorCodeList[err1][err2]) {
+            console.error('Error: ', errorCodeList[err1][err2], errMessage);
+        } else {
+            console.error('Unknown error, codes:', err1, err2, errMessage);
+        }
     }
 
     turnOn() {
@@ -157,7 +290,13 @@ export class ProjectorController {
     }
 
     parseResponse(response: Uint8Array): boolean {
-        let hex = toHexArray(response) as Array<number>;
+        console.log('Got data: ', byteToHexString(response));
+        var bytes = new Uint8Array(hex);
+        var hex = [];
+        response.map((byte) => {
+            hex.push((byte & 0xFF));
+            return undefined;
+        });
         let origin = hex.shift();
         let code: number = undefined;
         switch(origin) {
@@ -177,13 +316,24 @@ export class ProjectorController {
                             this.status.power.coolingProcess = (hex.shift() === 0x01) ? true : false;
                             this.status.power.powerOnOffProcess = (hex.shift() === 0x01) ? true : false;
                             this.status.power.operationStatus = operationStatus[hex.shift().toString(16)];
+                            console.log('Status requested, new status', projector.status);
                             return true;
                         }
                         return false;
                     default: 
                         return false;
                 }
-                break;
+            case 0x21:
+                code = hex.shift();
+                this.setId(hex.shift(), hex.shift());
+                switch(code) {
+                    case 0x98:
+                        // Only switch if successful
+                        this.status.picture.freeze = (hex.shift() === 0x00) ? !this.status.picture.freeze : this.status.picture.freeze;
+                        return true;
+                    default:
+                        return false;
+                }
             case 0x22:
                 code = hex.shift();
                 this.setId(hex.shift(), hex.shift());
@@ -216,6 +366,7 @@ export class ProjectorController {
                     default:
                         return false;
                 }
+            case 0xA1:
             case 0xA2:
             case 0xA0:
                 code = hex.shift();
@@ -226,6 +377,9 @@ export class ProjectorController {
                     message = 'UNKNOWN CODE - WRONG BYTE, missing 0x02';
                 }
                 switch(code) {
+                    case 0x01:
+                        message = 'Failed to power off projector';
+                        break;
                     case 0x10:
                         if(origin === 0xA2) {
                             message = 'Picture mute on failed';
@@ -251,6 +405,9 @@ export class ProjectorController {
                     case 0x85:
                         message = 'Settings request - response failed';
                         break;
+                    case 0x98:
+                        message = 'Freeze command failed';
+                        break;
                     default:
                         message = 'Unknown error - code not found';
                 }
@@ -273,14 +430,16 @@ export class ProjectorController {
 
 let projector = new ProjectorController();
 
-socket.on('data', function(data) {
-    console.log('Received: ', projector.parseResponse(data));
-    console.log(projector.status);
-	socket.destroy(); // kill client after server's response
-});
-
-socket.on('close', function() {
-	console.log('Connection closed');
-});
+if(socket) {
+    socket.on('data', function(data) {
+        console.log('Received: ', projector.parseResponse(data));
+        //console.log(projector.status);
+        //socket.destroy(); // kill client after server's response
+    });
+    
+    socket.on('close', function() {
+        console.log('Connection closed');
+    });
+}
 
 
